6c4300866cac7f6d8edafe0522a746e0f4a59784
Sema <sema@pyva.net>
2018-04-13 23:43:25 -0400

    Loading file

--- firefox/content.js
+++ firefox/content.js
@@ -9,17 +9,18 @@ var main = function() {
   var __mpeg = 'audio/mpeg';
   var __ogg = 'audio/ogg';
   var __wav = 'audio/wav';
+  var _all;
   function isTrue(str) {
     if (str == '') return true;
     str = str.toLowerCase();
     if (str == 'f' || str == 'false' || str == 0) return false;
     return !!str;
   }
-  function getBool(x, a) { 
+  function getBool(x, a) {
     if (x && x.attributes && x.attributes[a]) return isTrue(x.attributes[a].nodeValue);
     return false;
   }
-  function getAttr(x, a) { 
+  function getAttr(x, a) {
     if (x && x.attributes && x.attributes[a]) return x.attributes[a].nodeValue;
     return '';
   }
@@ -121,10 +122,56 @@ var main = function() {
         }
       }
     }
-console.log(all);
+    _all = all;
   }
+  function Player(x) {
+//console.log('x =', x);
+    this.src = x.src;
+    this.h = 40;
+    this.w = 270;
+  }
+  Player.prototype.create = function() {
+    var self = this;
+    var div = document.createElement('div');
+    div.title = 'Loading ' + this.src;
+    div.style.display = 'inline-block';
+    div.style.margin = '0px';
+    div.style.padding = '0px';
+    div.style.borderStyle = 'solid';
+    div.style.borderColor = '#f00';
+    div.style.borderWidth = '1px';
+    div.style.cursor = 'default';
+    div.style.width = this.w + 'px';
+    div.style.height = this.h + 'px';
+    document.body.appendChild(div);
+    self.div = div;
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4) {
+        if (this.status == 200) {
+          self.div.style.borderColor = '#0f0';
+          var r = xhttp.responseText;
+          self.data = '';
+          for (var i = 0; i < r.length; i++) self.data += String.fromCharCode(r.charCodeAt(i) & 0xff);
+          self.div.title = self.src;
+        }
+        else {
+          self.div.title = 'Cannot load ' + self.src;
+        }
+      }
+    };
+    xhttp.overrideMimeType("text/plain; charset=x-user-defined");
+    xhttp.open("GET", self.src, true);
+    xhttp.send();
+  }
+
   console.log('Initializing MIDI Player...');
   search();
+  for (var i in _all) {
+    var p = new Player(_all[i]);
+    p.create();
+  }
 };
 
 var script = document.createElement('script');
