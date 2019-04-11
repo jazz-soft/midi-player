958091e3f0d7c93362c1d6b4a9a054fb6f6d4d58
Sema <sema@pyva.net>
2019-04-10 22:58:50 -0400

    Updated dependencies

--- safari/midi-player.safariextension/xPlayer.js
+++ safari/midi-player.safariextension/xPlayer.js
@@ -155,7 +155,7 @@ function _Player() {
       if (window.FileReader) {
         self.fileBtn.off();
         self.fileBtn.div.addEventListener('click', function() { self.fileInput.click(); });
-        self.fileInput.addEventListener('change', function(e) { _stopProp(e); self.readFile(e.target.files[0]); });
+        self.fileInput.addEventListener('change', function(e) { _stopProp(e); if (e.target.files[0]) self.readFile(e.target.files[0]); });
         self.gui.addEventListener('drop', function(e) { _stopProp(e); self.fileBtn.off(); self.readFile(e.dataTransfer.files[0]); });
         self.gui.addEventListener('dragover', function(e) { _stopProp(e); self.fileBtn.on(); e.dataTransfer.dropEffect = 'copy'; });
         self.gui.addEventListener('dragexit', function(e) { _stopProp(e); self.fileBtn.off(); });
@@ -474,7 +474,7 @@ function _Player() {
         JZZ.lib.schedule(function() { self.load(smf); });
         if (self.linkBtn) self.setUrl('data:audio/midi;base64,' + JZZ.lib.toBase64(data), f.name);
       }
-      catch (err) {}
+      catch (err) { console.log(err.message); }
     };
     reader.readAsArrayBuffer(f);
   };
