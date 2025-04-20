827f6431290d48296aa8185f12ad70a73fa79a40
Sema <jazz-dev@jazz-soft.net>
2025-04-20 15:21:03 -0400

    Updated to manifest v.3

--- src/content.js
+++ src/content.js
@@ -1,3 +1,5 @@
 var script = document.createElement('script');
-script.src = browser.extension.getURL('inject.js');
+if (typeof browser == 'undefined') browser = chrome;
+script.src = browser.runtime.getURL('inject.js');
+script.onload = function() { script.remove(); }
 document.documentElement.appendChild(script);
